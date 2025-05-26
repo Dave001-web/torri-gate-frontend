69a13ee905b27c1df2c3cc4c40591e541e2991fd
Dave001-web <davidshun991@gmail.com>
2025-05-26 14:51:17 +0100

    feat: added profile photo

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,9 +1,49 @@
 import { createContext } from "react";
+import { useState, useEffect } from "react";
 
 export const appContext = createContext();
 
 const AppProvider = ({ children }) => {
-  return <appContext.Provider value={{}}>{children}</appContext.Provider>;
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState(null);
+  const [Loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    const storedToken = localStorage.getItem("accessToken");
+
+    if (storedToken && storedUser) {
+      setToken(storedToken);
+      setUser(JSON.parse(storedUser));
+    }
+    setLoading(false);
+  }, []);
+
+  const login = (accessToken, user) => {
+    setToken(accessToken);
+    setUser(user);
+    localStorage.setItem("accessToken", accessToken);
+    localStorage.setItem("user", JSON.stringify(user));
+  };
+  const logout = () => {
+    setUser(null);
+    setToken(null);
+    localStorage.removeItem("accessToken");
+    localStorage.removeItem("user");
+  };
+
+  const updateUser = (updatedUser) => {
+    setUser(updateUser);
+    localStorage.setItem("user", JSON.stringify(updatedUser));
+  };
+
+  return (
+    <appContext.Provider
+      value={{ login, logout, user, token, Loading, updateUser }}
+    >
+      {children}
+    </appContext.Provider>
+  );
 };
 
 export default AppProvider;
